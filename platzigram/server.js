74306df2dad4a51f5e5b9c3709fbe869419c9b83
Yamile Tabares <yamile08.lytm@gmail.com>
2018-02-19 14:51:47 -0500

    Obteniendo respuesta del servidor superagent y servidor Axios

--- platzigram/server.js
+++ platzigram/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.set('view engine', 'pug');//Para desirle a express utilizando node que nuestra aplicacion va utilizar un motor de vistas
+app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
@@ -11,11 +11,40 @@ app.get('/', function (req, res) {
 })
 
 app.get('/signup', function (req, res) {
-  res.render('index', { title: 'Platzigram - signup' });
+  res.render('index', { title: 'Platzigram - Signup' });
 })
 
 app.get('/signin', function (req, res) {
-  res.render('index', { title: 'Platzigram - signin' });
+  res.render('index', { title: 'Platzigram - Signin' });
+})
+
+app.get('/api/pictures', function (req, res, next) {
+  var pictures = [
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+      },
+      url: 'office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+      },
+      url: 'office.jpg',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+
+  setTimeout(function () {
+    res.send(pictures);  
+  }, 2000)
 })
 
 app.listen(3000, function (err) {
